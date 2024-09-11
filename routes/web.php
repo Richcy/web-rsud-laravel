69653e4401cc7eef7e02e99eaaa888ff64327210
Richcy Dian Sukma <richcyveliz@gmail.com>
2024-09-11 11:23:40 +0700

    Learning laravel layout: modify button and cards page

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,10 @@ Route::get('/backend', function () {
     return view('admin/index');
 });
 
+Route::get('/buttons', function () {
+    return view('admin/buttons');
+});
+
+Route::get('/cards', function () {
+    return view('admin/cards');
+});
